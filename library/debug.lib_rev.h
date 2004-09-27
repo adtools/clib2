51e20cee1de787ee1f1bbd686eb66a3276dd00e9
Thomas Frieden <thomas@friedenhq.org>
2004-09-27 15:11:20 +0000

    Bumped revision
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/trunk@14733 87f5fb63-7c3d-0410-a384-fd976d0f7a62

--- library/debug.lib_rev.h
+++ library/debug.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	175
-#define DATE		"10.9.2004"
-#define VERS		"debug.lib 1.175"
-#define VSTRING		"debug.lib 1.175 (10.9.2004)\r\n"
-#define VERSTAG		"\0$VER: debug.lib 1.175 (10.9.2004)"
+#define REVISION	176
+#define DATE		"27.9.2004"
+#define VERS		"debug.lib 1.176"
+#define VSTRING		"debug.lib 1.176 (27.9.2004)\r\n"
+#define VERSTAG		"\0$VER: debug.lib 1.176 (27.9.2004)"
--- library/stack.lib_rev.h
+++ library/stack.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	175
-#define DATE		"10.9.2004"
-#define VERS		"stack.lib 1.175"
-#define VSTRING		"stack.lib 1.175 (10.9.2004)\r\n"
-#define VERSTAG		"\0$VER: stack.lib 1.175 (10.9.2004)"
+#define REVISION	176
+#define DATE		"27.9.2004"
+#define VERS		"stack.lib 1.176"
+#define VSTRING		"stack.lib 1.176 (27.9.2004)\r\n"
+#define VERSTAG		"\0$VER: stack.lib 1.176 (27.9.2004)"
