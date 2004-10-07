aa3ccda8de5023f8c3da65a9d82243ab248069b4
Olaf Barthel <obarthel@gmx.net>
2004-10-07 14:58:03 +0000

    c.lib 1.178 (7.10.2004)
    - The OS4 version had floating point math support code enabled in all
      libraries and not just "libm.a". Fixed.
    
    
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/trunk@14750 87f5fb63-7c3d-0410-a384-fd976d0f7a62

--- library/c.lib_rev.h
+++ library/c.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	177
-#define DATE		"29.9.2004"
-#define VERS		"c.lib 1.177"
-#define VSTRING		"c.lib 1.177 (29.9.2004)\r\n"
-#define VERSTAG		"\0$VER: c.lib 1.177 (29.9.2004)"
+#define REVISION	178
+#define DATE		"7.10.2004"
+#define VERS		"c.lib 1.178"
+#define VSTRING		"c.lib 1.178 (7.10.2004)\r\n"
+#define VERSTAG		"\0$VER: c.lib 1.178 (7.10.2004)"
--- library/unix.lib_rev.h
+++ library/unix.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	177
-#define DATE		"29.9.2004"
-#define VERS		"unix.lib 1.177"
-#define VSTRING		"unix.lib 1.177 (29.9.2004)\r\n"
-#define VERSTAG		"\0$VER: unix.lib 1.177 (29.9.2004)"
+#define REVISION	178
+#define DATE		"7.10.2004"
+#define VERS		"unix.lib 1.178"
+#define VSTRING		"unix.lib 1.178 (7.10.2004)\r\n"
+#define VERSTAG		"\0$VER: unix.lib 1.178 (7.10.2004)"
