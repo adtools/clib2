56b0a74c61a8d353db645fa6511c565d97011b63
Olaf Barthel <obarthel@gmx.net>
2005-01-14 09:07:17 +0000

    - Added a missing definition to stdio_init_exit.c which is part of   the OS4 header files, but not of the older header file distributions.
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/trunk@14804 87f5fb63-7c3d-0410-a384-fd976d0f7a62

--- library/unix.lib_rev.h
+++ library/unix.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	185
-#define DATE		"2.1.2005"
-#define VERS		"unix.lib 1.185"
-#define VSTRING		"unix.lib 1.185 (2.1.2005)\r\n"
-#define VERSTAG		"\0$VER: unix.lib 1.185 (2.1.2005)"
+#define REVISION	186
+#define DATE		"14.1.2005"
+#define VERS		"unix.lib 1.186"
+#define VSTRING		"unix.lib 1.186 (14.1.2005)\r\n"
+#define VERSTAG		"\0$VER: unix.lib 1.186 (14.1.2005)"
