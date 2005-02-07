a626336b7b0bea111399c0e018a0f099a1983bc0
Olaf Barthel <obarthel@gmx.net>
2005-02-07 10:52:32 +0000

    - Tweaked the build makefiles to produce fewer meaningless warnings.
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/trunk@14828 87f5fb63-7c3d-0410-a384-fd976d0f7a62

--- library/amiga.lib_rev.h
+++ library/amiga.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	187
-#define DATE		"29.1.2005"
-#define VERS		"amiga.lib 1.187"
-#define VSTRING		"amiga.lib 1.187 (29.1.2005)\r\n"
-#define VERSTAG		"\0$VER: amiga.lib 1.187 (29.1.2005)"
+#define REVISION	188
+#define DATE		"7.2.2005"
+#define VERS		"amiga.lib 1.188"
+#define VSTRING		"amiga.lib 1.188 (7.2.2005)\r\n"
+#define VERSTAG		"\0$VER: amiga.lib 1.188 (7.2.2005)"
--- library/debug.lib_rev.h
+++ library/debug.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	187
-#define DATE		"29.1.2005"
-#define VERS		"debug.lib 1.187"
-#define VSTRING		"debug.lib 1.187 (29.1.2005)\r\n"
-#define VERSTAG		"\0$VER: debug.lib 1.187 (29.1.2005)"
+#define REVISION	188
+#define DATE		"7.2.2005"
+#define VERS		"debug.lib 1.188"
+#define VSTRING		"debug.lib 1.188 (7.2.2005)\r\n"
+#define VERSTAG		"\0$VER: debug.lib 1.188 (7.2.2005)"
