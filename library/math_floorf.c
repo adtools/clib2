505c23237a8d2633e91f1f6af486de59b4037b65
Olaf Barthel <obarthel@gmx.net>
2005-05-29 11:19:01 +0000

    - Added skeleton code for acosf(), acosh(), acoshf(), asinf(), asinh(),   asinhf(), atan2f(), atanf(), atanh(), atanhf(), cbrt(), cbrtf(),   ceilf(), cosf(), coshf(), erf(), erfc(), erfcf(), erff(),   exp2(), exp2f(), expf(), expm1(), expm1f(), fdim(), fdimf(),   floorf(), fma(), fmaf(), fmax(), fmaxf(), fmin(), fminf(),   fmodf(), frexpf(), hypotf(), ilogb(), ilogbf(), ldexpf(), lgamma(),   lgammaf(), log10f(), log1p(), log1pf(), log2(), log2f(), logbf(),   logf(), lrint(), lrintf(), lround(), lroundf(), modff(), nearbyint(),   nearbyintf(), powf(), remainder(), remainderf(), remquo(), remquof(), round(),   roundf(), scalbn(), scalbnf(), sinf(), sinhf(), sqrtf(), tanf(),   tanhf(), tgamma(), tgammaf(), trunc() and truncf(), to be filled in   later...
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/trunk@14961 87f5fb63-7c3d-0410-a384-fd976d0f7a62

---
+++ library/math_floorf.c
@@ -0,0 +1,53 @@
+/*
+ * $Id: math_floorf.c,v 1.1 2005-05-29 11:19:01 obarthel Exp $
+ *
+ * :ts=4
+ *
+ * Portable ISO 'C' (1994) runtime library for the Amiga computer
+ * Copyright (c) 2002-2005 by Olaf Barthel <olsen@sourcery.han.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Neither the name of Olaf Barthel nor the names of contributors
+ *     may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _MATH_HEADERS_H
+#include "math_headers.h"
+#endif /* _MATH_HEADERS_H */
+
+/****************************************************************************/
+
+#if defined(FLOATING_POINT_SUPPORT)
+
+/****************************************************************************/
+
+float
+floorf(float x)
+{
+	/* ZZZ unimplemented */
+	return(0);
+}
+
+/****************************************************************************/
+
+#endif /* FLOATING_POINT_SUPPORT */
