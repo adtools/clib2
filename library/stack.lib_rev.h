6245dccb4b3bccbe80184b454318b73dc01d0fb2
Olaf Barthel <obarthel@gmx.net>
2005-07-04 10:25:34 +0000

    This commit was manufactured by cvs2svn to create tag 'LIB_V1_2'.
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/tags/LIB_V1_2@15003 87f5fb63-7c3d-0410-a384-fd976d0f7a62

--- library/stack.lib_rev.h
+++
@@ -1,6 +0,0 @@
-#define VERSION		1
-#define REVISION	193
-#define DATE		"4.6.2005"
-#define VERS		"stack.lib 1.193"
-#define VSTRING		"stack.lib 1.193 (4.6.2005)\r\n"
-#define VERSTAG		"\0$VER: stack.lib 1.193 (4.6.2005)"
--- library/unix.lib_rev.h
+++
@@ -1,6 +0,0 @@
-#define VERSION		1
-#define REVISION	193
-#define DATE		"4.6.2005"
-#define VERS		"unix.lib 1.193"
-#define VSTRING		"unix.lib 1.193 (4.6.2005)\r\n"
-#define VERSTAG		"\0$VER: unix.lib 1.193 (4.6.2005)"
