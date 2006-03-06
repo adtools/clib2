0266e8c0adfaeb777425541c91fee2cfdaf27296
Olaf Barthel <obarthel@gmx.net>
2006-03-06 11:10:59 +0000

    - Preparations for release...
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/trunk@15086 87f5fb63-7c3d-0410-a384-fd976d0f7a62

--- library/m881.lib_rev.h
+++ library/m881.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	198
-#define DATE		"11.12.2005"
-#define VERS		"m881.lib 1.198"
-#define VSTRING		"m881.lib 1.198 (11.12.2005)\r\n"
-#define VERSTAG		"\0$VER: m881.lib 1.198 (11.12.2005)"
+#define REVISION	199
+#define DATE		"6.3.2006"
+#define VERS		"m881.lib 1.199"
+#define VSTRING		"m881.lib 1.199 (6.3.2006)\r\n"
+#define VERSTAG		"\0$VER: m881.lib 1.199 (6.3.2006)"
