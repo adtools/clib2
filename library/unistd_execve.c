40d64e14134333a12335a79645f1a7d2e61a50d5
Olaf Barthel <obarthel@gmx.net>
2006-08-06 08:31:06 +0000

    - Added the missing fdim() and fdimf() functions to the build   makefiles. The OS4 build makefile was missing lrint() and   lrintf(). Ouch.
    - execve() now checks if an abort signal arrives while it's trying
      to read the first line of a script, or looking for an executable
      on an assignment path.
    
    
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/trunk@15123 87f5fb63-7c3d-0410-a384-fd976d0f7a62

--- library/unistd_execve.c
+++ library/unistd_execve.c
@@ -1,5 +1,5 @@
 /*
- * $Id: unistd_execve.c,v 1.7 2006-08-06 08:15:42 obarthel Exp $
+ * $Id: unistd_execve.c,v 1.8 2006-08-06 08:31:06 obarthel Exp $
  *
  * :ts=4
  *
@@ -134,6 +134,13 @@ get_first_script_line(const char * path,char ** line_ptr)
 		if(script_line_length + 2 > script_line_size)
 		{
 			char * new_script_line;
+
+			/* Give the user a chance to bail out. */
+			if(__check_abort_enabled && (SetSignal(0,0) & __break_signal_mask) != 0)
+			{
+				__set_errno(EAGAIN);
+				goto out;
+			}
 
 			/* Always reserve a little more memory than needed,
 			   and one extra byte to allow us to to NUL-terminate
@@ -310,6 +317,13 @@ find_command(const char * path,struct program_info ** result_ptr)
 
 		do
 		{
+			/* Give the user a chance to bail out. */
+			if(__check_abort_enabled && (SetSignal(0,0) & __break_signal_mask) != 0)
+			{
+				error = EAGAIN;
+				break;
+			}
+
 			dvp = GetDeviceProc((STRPTR)path,dvp);
 			if(dvp != NULL)
 			{
@@ -795,6 +809,9 @@ execve(const char *path, char *const argv[], char *const envp[])
 	/* If things went well, we can actually quit now. */
 	if(success)
 		__execve_exit(result);
+
+	/* Just in case somebody wants to quit... */
+	__check_abort();
 
 	return(result);
 }
