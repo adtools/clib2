3541b011a64df13cd18cea3d8733c9aec2d78b25
Olaf Barthel <obarthel@gmx.net>
2006-09-21 08:48:19 +0000

    - Preparations for release...
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/trunk@15139 87f5fb63-7c3d-0410-a384-fd976d0f7a62

--- library/net.lib_rev.h
+++ library/net.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	200
-#define DATE		"17.4.2006"
-#define VERS		"net.lib 1.200"
-#define VSTRING		"net.lib 1.200 (17.4.2006)\r\n"
-#define VERSTAG		"\0$VER: net.lib 1.200 (17.4.2006)"
+#define REVISION	201
+#define DATE		"21.9.2006"
+#define VERS		"net.lib 1.201"
+#define VSTRING		"net.lib 1.201 (21.9.2006)\r\n"
+#define VERSTAG		"\0$VER: net.lib 1.201 (21.9.2006)"
--- library/unix.lib_rev.h
+++ library/unix.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	200
-#define DATE		"17.4.2006"
-#define VERS		"unix.lib 1.200"
-#define VSTRING		"unix.lib 1.200 (17.4.2006)\r\n"
-#define VERSTAG		"\0$VER: unix.lib 1.200 (17.4.2006)"
+#define REVISION	201
+#define DATE		"21.9.2006"
+#define VERS		"unix.lib 1.201"
+#define VSTRING		"unix.lib 1.201 (21.9.2006)\r\n"
+#define VERSTAG		"\0$VER: unix.lib 1.201 (21.9.2006)"
