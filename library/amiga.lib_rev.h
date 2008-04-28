6a149abc57d88ca39c16c02ba74af7b22856a874
Olaf Barthel <obarthel@gmx.net>
2008-04-28 15:42:13 +0000

    - Final checkin prior to upcoming release.
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/trunk@15192 87f5fb63-7c3d-0410-a384-fd976d0f7a62

--- library/amiga.lib_rev.h
+++ library/amiga.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	202
-#define DATE		"16.1.2007"
-#define VERS		"amiga.lib 1.202"
-#define VSTRING		"amiga.lib 1.202 (16.1.2007)\r\n"
-#define VERSTAG		"\0$VER: amiga.lib 1.202 (16.1.2007)"
+#define REVISION	203
+#define DATE		"28.4.2008"
+#define VERS		"amiga.lib 1.203"
+#define VSTRING		"amiga.lib 1.203 (28.4.2008)\r\n"
+#define VERSTAG		"\0$VER: amiga.lib 1.203 (28.4.2008)"
--- library/m.lib_rev.h
+++ library/m.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	202
-#define DATE		"16.1.2007"
-#define VERS		"m.lib 1.202"
-#define VSTRING		"m.lib 1.202 (16.1.2007)\r\n"
-#define VERSTAG		"\0$VER: m.lib 1.202 (16.1.2007)"
+#define REVISION	203
+#define DATE		"28.4.2008"
+#define VERS		"m.lib 1.203"
+#define VSTRING		"m.lib 1.203 (28.4.2008)\r\n"
+#define VERSTAG		"\0$VER: m.lib 1.203 (28.4.2008)"
