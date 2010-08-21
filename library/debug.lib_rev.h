6bc8108a39d66d616798d279a19c9aa8fe7b09a3
Olaf Barthel <obarthel@gmx.net>
2010-08-21 11:39:10 +0000

    - Version bump to 1.205
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/trunk@15208 87f5fb63-7c3d-0410-a384-fd976d0f7a62

--- library/debug.lib_rev.h
+++ library/debug.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	204
-#define DATE		"11.11.2008"
-#define VERS		"debug.lib 1.204"
-#define VSTRING		"debug.lib 1.204 (11.11.2008)\r\n"
-#define VERSTAG		"\0$VER: debug.lib 1.204 (11.11.2008)"
+#define REVISION	205
+#define DATE		"21.8.2010"
+#define VERS		"debug.lib 1.205"
+#define VSTRING		"debug.lib 1.205 (21.8.2010)\r\n"
+#define VERSTAG		"\0$VER: debug.lib 1.205 (21.8.2010)"
--- library/unix.lib_rev.h
+++ library/unix.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	204
-#define DATE		"11.11.2008"
-#define VERS		"unix.lib 1.204"
-#define VSTRING		"unix.lib 1.204 (11.11.2008)\r\n"
-#define VERSTAG		"\0$VER: unix.lib 1.204 (11.11.2008)"
+#define REVISION	205
+#define DATE		"21.8.2010"
+#define VERS		"unix.lib 1.205"
+#define VSTRING		"unix.lib 1.205 (21.8.2010)\r\n"
+#define VERSTAG		"\0$VER: unix.lib 1.205 (21.8.2010)"
