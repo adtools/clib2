bdfdd84e38dfcf59e195c1b5236c9cb1a608d456
Olaf Barthel <obarthel@gmx.net>
2015-04-24 13:56:39 +0000

    - Bumped math library versions, due to vfscanf() changes.
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/clib2/trunk@15214 87f5fb63-7c3d-0410-a384-fd976d0f7a62

--- library/m881.lib_rev.h
+++ library/m881.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	205
-#define DATE		"21.8.2010"
-#define VERS		"m881.lib 1.205"
-#define VSTRING		"m881.lib 1.205 (21.8.2010)\r\n"
-#define VERSTAG		"\0$VER: m881.lib 1.205 (21.8.2010)"
+#define REVISION	206
+#define DATE		"24.4.2015"
+#define VERS		"m881.lib 1.206"
+#define VSTRING		"m881.lib 1.206 (24.4.2015)\r\n"
+#define VERSTAG		"\0$VER: m881.lib 1.206 (24.4.2015)"
