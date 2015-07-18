a1b46cb3d46800cc93259d54cd01f00c2a446c48
Olaf Barthel <obarthel@gmx.net>
2015-07-18 10:01:10 +0200

    Added the missing library revision information pertaining to clib2 1.206.

--- library/net.lib_rev.h
+++ library/net.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	205
-#define DATE		"21.8.2010"
-#define VERS		"net.lib 1.205"
-#define VSTRING		"net.lib 1.205 (21.8.2010)\r\n"
-#define VERSTAG		"\0$VER: net.lib 1.205 (21.8.2010)"
+#define REVISION	206
+#define DATE		"24.4.2015"
+#define VERS		"net.lib 1.206"
+#define VSTRING		"net.lib 1.206 (24.4.2015)\r\n"
+#define VERSTAG		"\0$VER: net.lib 1.206 (24.4.2015)"
--- library/unix.lib_rev.h
+++ library/unix.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	205
-#define DATE		"21.8.2010"
-#define VERS		"unix.lib 1.205"
-#define VSTRING		"unix.lib 1.205 (21.8.2010)\r\n"
-#define VERSTAG		"\0$VER: unix.lib 1.205 (21.8.2010)"
+#define REVISION	206
+#define DATE		"24.4.2015"
+#define VERS		"unix.lib 1.206"
+#define VSTRING		"unix.lib 1.206 (24.4.2015)\r\n"
+#define VERSTAG		"\0$VER: unix.lib 1.206 (24.4.2015)"
