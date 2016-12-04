4c54ee3f2d5a4bca79b98fb43bcbbe47f7930b17
obarthel <obarthel@gmx.net>
2016-12-04 11:14:17 +0100

    Version bump

--- library/amiga.lib_rev.h
+++ library/amiga.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	212
-#define DATE		"27.11.2016"
-#define VERS		"amiga.lib 1.212"
-#define VSTRING		"amiga.lib 1.212 (27.11.2016)\r\n"
-#define VERSTAG		"\0$VER: amiga.lib 1.212 (27.11.2016)"
+#define REVISION	213
+#define DATE		"4.12.2016"
+#define VERS		"amiga.lib 1.213"
+#define VSTRING		"amiga.lib 1.213 (4.12.2016)\r\n"
+#define VERSTAG		"\0$VER: amiga.lib 1.213 (4.12.2016)"
--- library/unix.lib_rev.h
+++ library/unix.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	212
-#define DATE		"27.11.2016"
-#define VERS		"unix.lib 1.212"
-#define VSTRING		"unix.lib 1.212 (27.11.2016)\r\n"
-#define VERSTAG		"\0$VER: unix.lib 1.212 (27.11.2016)"
+#define REVISION	213
+#define DATE		"4.12.2016"
+#define VERS		"unix.lib 1.213"
+#define VSTRING		"unix.lib 1.213 (4.12.2016)\r\n"
+#define VERSTAG		"\0$VER: unix.lib 1.213 (4.12.2016)"
