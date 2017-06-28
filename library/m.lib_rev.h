6a1fd36b3a43ad6193f16374a05ac9aa1d26fb31
Steven Solie <steven@solie.ca>
2017-06-27 21:31:27 -0600

    Revision bump.

--- library/m.lib_rev.h
+++ library/m.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	214
-#define DATE		"27.4.2017"
-#define VERS		"m.lib 1.214"
-#define VSTRING		"m.lib 1.214 (27.4.2017)\r\n"
-#define VERSTAG		"\0$VER: m.lib 1.214 (27.4.2017)"
+#define REVISION	215
+#define DATE		"26.6.2017"
+#define VERS		"m.lib 1.215"
+#define VSTRING		"m.lib 1.215 (26.6.2017)\r\n"
+#define VERSTAG		"\0$VER: m.lib 1.215 (26.6.2017)"
