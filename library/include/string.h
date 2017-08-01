c76bf8e20b3daa9dc92aa9c63b121b02f2cb34e4
Henning Nielsen Lund <hnl_dk@amigaos.dk>
2017-08-01 21:28:07 +0200

    add strnlen()

--- library/include/string.h
+++ library/include/string.h
@@ -62,6 +62,7 @@ extern int strcmp(const char *s1, const char * s2);
 extern int strncmp(const char *s1, const char *s2, size_t n);
 extern char *strcpy(char *dest, const char *src);
 extern char *strncpy(char *dest, const char *src, size_t n);
+extern size_t strnlen(const char *s, size_t maxlen);
 extern size_t strlen(const char *s);
 extern char *strchr(const char *s, int c);
 extern char *strrchr(const char *s, int c);
