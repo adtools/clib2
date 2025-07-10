a1f85a9c7fc6a17b2e46394e50f90c1497b06fd3
Olaf Barthel <o.barthel@logical-line.de>
2025-07-10 14:21:30 +0200

    First preparations for a future clib2 release

--- library/amiga.lib_rev.h
+++ library/amiga.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	215
-#define DATE		"26.6.2017"
-#define VERS		"amiga.lib 1.215"
-#define VSTRING		"amiga.lib 1.215 (26.6.2017)\r\n"
-#define VERSTAG		"\0$VER: amiga.lib 1.215 (26.6.2017)"
+#define REVISION	216
+#define DATE		"10.7.2025"
+#define VERS		"amiga.lib 1.216"
+#define VSTRING		"amiga.lib 1.216 (10.7.2025)\r\n"
+#define VERSTAG		"\0$VER: amiga.lib 1.216 (10.7.2025)"
--- library/c.lib_rev.h
+++ library/c.lib_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	215
-#define DATE		"26.6.2017"
-#define VERS		"c.lib 1.215"
-#define VSTRING		"c.lib 1.215 (26.6.2017)\r\n"
-#define VERSTAG		"\0$VER: c.lib 1.215 (26.6.2017)"
+#define REVISION	217
+#define DATE		"10.7.2025"
+#define VERS		"c.lib 1.217"
+#define VSTRING		"c.lib 1.217 (10.7.2025)\r\n"
+#define VERSTAG		"\0$VER: c.lib 1.217 (10.7.2025)"
